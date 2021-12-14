98d635025fa3e5f6f4811e5091ce2904eea832d1
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2021-12-14 11:33:11 +0530

    fix : validation and UI bugs
    1. Fixed validating github accounts before displaying.
    2. Fixed appending of projects list with previous searched usernames

--- script.js
+++ script.js
@@ -6,12 +6,23 @@ const repoHeading = document.querySelector("#repo-heading");
 
 avatar.style.display = "none";
 repoHeading.style.display = "none";
+
 var url = "https://api.github.com/users/";
 function getInfo() {
   let x = userName.value;
   let urlx = url + x;
+  output.innerHTML = " ";
   fetch(urlx)
-    .then((res) => res.json())
+    .then((res) => {
+      if (res.status == 200) {
+        return res.json();
+      } else {
+        avatar.style.display = "none";
+        avatar.src = "";
+        repoHeading.style.display = "none";
+        output.innerHTML = `<h2>Invalid UserName</h2>`;
+      }
+    })
     .then((data) => {
       avatar.style.display = "block";
       avatar.src = data.avatar_url;
